1e405c6f956f37a32e305a9a9a760cf8f0829501
appolinaria07 <paulyna.k@gmail.com>
2023-04-19 17:47:03 -0700

    init

---
+++ js/modules/init.js
@@ -0,0 +1,11 @@
+export async function fetchEmployees(){
+    try {
+        const response = await fetch('././/data/employees.json')
+        const employees = await response.json()
+        return employees
+    }
+    catch (e) {
+        console.log(e.message)
+    }
+}
+// export {fetchEmployees};
